f78f0df7b28619853d9c351e7c064d3301c9590b
JasonHome123 <yf9000555@gmail.com>
2016-12-28 16:14:44 +0800

    Honda爬蟲完成

--- CrawlerHonda.py
+++ CrawlerHonda.py
@@ -4,33 +4,39 @@ from ReadTxt_edit import *
 from bs4 import BeautifulSoup
 from tqdm import *
 import os,io,pdb
-
-
 TxtContant=[]
-TxtContant=["广东,阳江"]#Read_txt("C:/Python27/Scripts/Crawler/CrawlerHonda/City.txt")
-WriteFile=Write_txt("C:/Python27/Scripts/Crawler/CrawlerHonda/Honda_Store_Result.txt")
-for cityTxt in TxtContant:
+TxtContant=Read_txt("C:/Python27/Scripts/Crawler/CrawlerHonda/City.txt")#["广东,阳江"]
+WriteFile=Write_txt("C:/Python27/Scripts/Crawler/CrawlerHonda/Honda_Store_Result1.txt")
+for cityTxt in tqdm(TxtContant):
 	WriteTxtStr=""
-	province,city=cityTxt.split(",")
+	try:
+		province,city=cityTxt.split(",")
+	except ValueError: #遇到香港特別行政區時就跳過.
+		continue #跳過這次循環
 	url="http://www.ghac.cn/buy/tools/find-dealer?d=ws&type=finddealers&province="+province+"&city="+city
-	r = requests.get(url)
+	while True:
+		try:
+			r = requests.get(url,timeout=30)
+		except requests.exceptions.Timeout:
+			print "Timeout"
+			pass
+		else:
+			break #離開迴圈
+
 	soup = BeautifulSoup(r.text,"html.parser")
 	for Store in soup.find_all("tr"):
 		try:
 			WriteTxtStr+=province+","
 			WriteTxtStr+=city+","
-			WriteTxtStr+=str(Store.a).decode("big5").split(">")[1].split("<")[0].replace("\n","").replace("\r","").replace(" ","")+","
-			print WriteTxtStr
+			WriteTxtStr+=str(Store.a).decode("utf-8").split(">")[1].split("<")[0].replace("\n","").replace("\r","").replace(" ","")+","
 			WriteTxtStr+=str(Store.find("td",{"class":"item2"})).decode("utf-8").split(">")[1].split("<")[0].replace("\n","").replace("\r","").replace(" ","")+","
-			print WriteTxtStr
-			WriteTxtStr+=str(Store.find("td",{"class":"item3"})).split(">")[1].split("<")[0].replace("\n","").replace("\r","").replace(" ","")
-			print WriteTxtStr
-			WriteFile.Write_Action(WriteTxtStr)
+			WriteTxtStr+=str(Store.find("td",{"class":"item3"})).decode("utf-8").split(">")[1].split("<")[0].replace("\n","").replace("\r","").replace(" ","")
+			WriteFile.Write_Action(WriteTxtStr.encode("utf-8"))
 			WriteFile.Write_Action("\n")
+			WriteFile.CloseFile()
 			pass
 		except IndexError:
 			pass
 		finally:
 			WriteTxtStr=""
 
-WriteFile.CloseFile()
